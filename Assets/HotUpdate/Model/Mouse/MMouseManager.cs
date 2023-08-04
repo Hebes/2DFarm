d581006ded3cc6233f2e4a88ee8619c933748ee2
Hebes <1607388033@qq.com>
2023-08-04 22:40:20 +0800

    更新

---
+++ Assets/HotUpdate/Model/Mouse/MMouseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*--------脚本描述-----------
+				
+电子邮箱：
+	1607388033@qq.com
+作者:
+	暗沉
+描述:
+    鼠标样式管理
+
+-----------------------*/
+
+namespace ACFrameworkCore
+{
+    public class MMouseManager
+    {
+    }
+}

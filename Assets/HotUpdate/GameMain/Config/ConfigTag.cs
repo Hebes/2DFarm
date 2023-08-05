08aeda6c6f5c358d803e25c5fa52acbc47840cfe
Hebes <1607388033@qq.com>
2023-08-05 20:49:26 +0800

    更新Tag标签获取

---
+++ Assets/HotUpdate/GameMain/Config/ConfigTag.cs
@@ -0,0 +1,13 @@
+namespace ACFrameworkCore
+{
+    public class ConfigTag
+    {
+        public const string TagUntagged = "Untagged";
+        public const string TagRespawn = "Respawn";
+        public const string TagFinish = "Finish";
+        public const string TagEditorOnly = "EditorOnly";
+        public const string TagMainCamera = "MainCamera";
+        public const string TagPlayer = "Player";
+        public const string TagGameController = "GameController";
+    }
+}
